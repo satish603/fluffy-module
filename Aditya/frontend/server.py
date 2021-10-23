d1f99465936ae49b34ee46ec824a0c6da37be729
satish chauhan <satishchauhan603@gmail.com>
2021-10-23 15:28:44 +0530

    dynamic

--- Aditya/frontend/server.py
+++ Aditya/frontend/server.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template, request
+import PyPDF2
+import re
+import urllib.request
+import json
+from flask import Flask, render_template, request,jsonify 
 
 
 app = Flask(__name__)
@@ -16,7 +20,18 @@ def index1():
 @app.route('/', methods=['POST'])
 def getvalue():
     search = request.form['search']
-    return render_template('data.html', s=search)
+    try:
+        from googlesearch import search
+    except ImportError: 
+        print("No module named 'google' found")
+    name = request.form['search']
+# to search
+    query = name+" filetype:pdf"
+    print(query)
+    ar = [] 
+    for j in search(query, tld="co.in", num=10, stop=3, pause=2):
+        ar.append(j)
+    return render_template('data.html', s=json.dumps(ar),r=jsonify(ar))
 
 
 if __name__ == '__main__':
